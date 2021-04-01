8de1f0b3f74a2247e54cd533c898c737e8962b40
Francesco-Grilli <66123653+Francesco-Grilli@users.noreply.github.com>
2021-04-01 09:44:07 +0200

    Added the Chest and ChestTest class. Need still some test

---
+++ src/main/java/it/polimi/ingsw/GrilliMannarino/Chest.java
@@ -0,0 +1,53 @@
+package it.polimi.ingsw.GrilliMannarino;
+
+import it.polimi.ingsw.GrilliMannarino.GameData.Resource;
+
+import java.util.HashMap;
+
+public class Chest {
+    private HashMap<Resource, Integer> resources = new HashMap<>();
+
+
+    public HashMap<Resource, Integer> getResources(){
+        return resources;
+    }
+
+    public void addResources(HashMap<Resource, Integer> input){
+        for(Resource current : input.keySet()){
+            if(resources.get(current) == null)
+                resources.put(current, input.get(current));
+            else
+                resources.put(current, resources.get(current)+input.get(current));
+        }
+    }
+
+    public boolean removeResources(HashMap<Resource, Integer> input){
+        boolean check = true;
+        HashMap<Resource, Integer> resourceCopy;
+        try {
+            resourceCopy = new HashMap<>(resources);
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+            return false;
+        }
+
+        for(Resource current : input.keySet()){
+            if(resourceCopy.get(current)!=null){
+                if(resourceCopy.get(current) > input.get(current))
+                    resourceCopy.put(current, resourceCopy.get(current) - input.get(current));
+                else
+                    check=false;
+            }
+            else
+                check=false;
+        }
+
+        if(check)
+            resources=resourceCopy;
+        return check;
+    }
+
+
+
+}
---
+++ src/test/java/it/polimi/ingsw/GrilliMannarino/ChestTest.java
@@ -0,0 +1,96 @@
+package it.polimi.ingsw.GrilliMannarino;
+
+import it.polimi.ingsw.GrilliMannarino.GameData.Resource;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChestTest {
+
+    @Test
+    public void addElement(){
+        Chest chest = new Chest();
+        HashMap<Resource, Integer> elem = new HashMap<>();
+        elem.put(Resource.COIN, 10);
+        elem.put(Resource.SERVANT, 15);
+        chest.addResources(elem);
+        HashMap<Resource, Integer> retValue;
+        retValue=chest.getResources();
+        assertEquals(15, (int) retValue.get(Resource.SERVANT));
+        assertEquals(10, (int) retValue.get(Resource.COIN));
+    }
+
+    @Test
+    public void doubleAddElement(){
+        Chest chest = new Chest();
+        HashMap<Resource, Integer> elem = new HashMap<>();
+        elem.put(Resource.COIN, 10);
+        elem.put(Resource.SERVANT, 15);
+        chest.addResources(elem);
+        HashMap<Resource, Integer> retValue;
+        retValue=chest.getResources();
+        assertEquals(15, (int) retValue.get(Resource.SERVANT));
+        assertEquals(10, (int) retValue.get(Resource.COIN));
+        elem = new HashMap<>();
+        elem.put(Resource.SHIELD, 100);
+        elem.put(Resource.STONE, 2000);
+        chest.addResources(elem);
+        retValue=chest.getResources();
+        assertEquals(15, (int) retValue.get(Resource.SERVANT));
+        assertEquals(10, (int) retValue.get(Resource.COIN));
+        assertEquals(2000, (int) retValue.get(Resource.STONE));
+        assertEquals(100, (int) retValue.get(Resource.SHIELD));
+    }
+
+    @Test
+    public void updateElement(){
+        Chest chest = new Chest();
+        HashMap<Resource, Integer> elem = new HashMap<>();
+        elem.put(Resource.COIN, 10);
+        elem.put(Resource.SERVANT, 15);
+        chest.addResources(elem);
+        HashMap<Resource, Integer> retValue;
+        retValue=chest.getResources();
+        assertEquals(15, (int) retValue.get(Resource.SERVANT));
+        assertEquals(10, (int) retValue.get(Resource.COIN));
+        elem = new HashMap<>();
+        elem.put(Resource.COIN, 20);
+        elem.put(Resource.SERVANT, 50);
+        chest.addResources(elem);
+        retValue=chest.getResources();
+        assertEquals(65, (int) retValue.get(Resource.SERVANT));
+        assertEquals(30, (int) retValue.get(Resource.COIN));
+    }
+
+    @Test
+    public void removeElementOK(){
+        Chest chest = new Chest();
+        HashMap<Resource, Integer> elem = new HashMap<>();
+        elem.put(Resource.COIN, 10);
+        elem.put(Resource.SERVANT, 15);
+        chest.addResources(elem);
+        HashMap<Resource, Integer> retValue;
+        retValue=chest.getResources();
+        assertEquals(15, (int) retValue.get(Resource.SERVANT));
+        assertEquals(10, (int) retValue.get(Resource.COIN));
+        elem.clear();
+        elem.put(Resource.SERVANT, 5);
+        elem.put(Resource.COIN, 7);
+        assertTrue(chest.removeResources(elem));
+        retValue=chest.getResources();
+        assertEquals(10, (int) retValue.get(Resource.SERVANT));
+        assertEquals(3, (int) retValue.get(Resource.COIN));
+        elem.clear();
+        elem.put(Resource.SERVANT, 1);
+        elem.put(Resource.COIN, 1);
+        assertTrue(chest.removeResources(elem));
+        retValue=chest.getResources();
+        assertEquals(9, (int) retValue.get(Resource.SERVANT));
+        assertEquals(2, (int) retValue.get(Resource.COIN));
+    }
+
+
+}
