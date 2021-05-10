58ea6e053d7fb3146cffea4cb25db813f38c7f94
manna <mannarinoandrea98@gmail.com>
2021-05-10 21:18:08 +0200

    more saving methods

--- src/main/java/it/polimi/ingsw/GrilliMannarino/BoardSinglePlayer.java
+++ src/main/java/it/polimi/ingsw/GrilliMannarino/BoardSinglePlayer.java
@@ -2,9 +2,40 @@ package it.polimi.ingsw.GrilliMannarino;
 
 public class BoardSinglePlayer extends Board{
 
-    public BoardSinglePlayer(Player player, CardMarketBoardInterface cardMarket, MarbleMarketBoardInterface marbleMarket) {
+    private PopeLineSingle popelineLorenzo;
+    private CardMarketBoardInterfaceSingle cardMarketSingle;
+
+    public BoardSinglePlayer(Player player, CardMarketBoardInterfaceSingle cardMarket, MarbleMarketBoardInterface marbleMarket) {
         super(player, cardMarket, marbleMarket);
-        this.popeLine = new PopeLineSingle();
+        this.popelineLorenzo = new PopeLineSingle();
+        this.popeLine = popelineLorenzo;
+    }
+
+    public boolean addLorenzoFaith(){
+        try{
+            return ((PopeLineSingle) popeLine).addLorenzoFaith();
+        }catch(ClassCastException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean doubleAddLorenzoFaith(){
+        try{
+            return ((PopeLineSingle) popeLine).doubleAddLorenzoFaith();
+        }catch (ClassCastException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public int getLorenzoFaith(){
+        try{
+            return ((PopeLineSingle) popeLine).getLorenzoFaith();
+        }catch(ClassCastException e){
+            e.printStackTrace();
+        }
+        return -1;
     }
 
 }
