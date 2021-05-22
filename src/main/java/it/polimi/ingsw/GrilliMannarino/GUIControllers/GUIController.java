0a769e1266f1bc2cf5e522fdf06cf91227e895ab
manna <mannarinoandrea98@gmail.com>
2021-05-22 10:55:11 +0200

    fxml different method to switch scene to fully test i believe it works

--- src/main/java/it/polimi/ingsw/GrilliMannarino/GUIControllers/GUIController.java
+++ src/main/java/it/polimi/ingsw/GrilliMannarino/GUIControllers/GUIController.java
@@ -3,21 +3,25 @@ package it.polimi.ingsw.GrilliMannarino.GUIControllers;
 import it.polimi.ingsw.GrilliMannarino.ClientController;
 import it.polimi.ingsw.GrilliMannarino.GUIView;
 import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class GUIController extends Application implements GUIControllerInterface {
+public class GUIController extends Application implements GUIControllerInterface, SmallController {
 
   private GUIView requestHandler;
   HashMap<String, String> sceneMap;
   SmallController activeController;
+  public BorderPane background;
 
   public GUIController(){
     sceneMap = new HashMap<>();
@@ -44,17 +48,22 @@ public class GUIController extends Application implements GUIControllerInterface
     this.requestHandler = view;
     setUpScene();
 
-    FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Welcome.fxml"));
+    FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Background.fxml"));
     Parent root = null;
     try {
       root = loader.load();
+      background = (BorderPane) root;
       SmallController wc = loader.getController();
       this.activeController = wc;
     } catch (IOException e) {
       e.printStackTrace();
     }
+    Scene scene = new Scene(root);
+    FXMLLoader object = new FXMLLoader(getClass().getClassLoader().getResource("Board.fxml"));
+    Pane pane = object.load();
+    background.setCenter(pane);
     stage.setTitle("prova");
-    stage.setScene(new Scene(root));
+    stage.setScene(scene);
     stage.show();
 
 
@@ -82,9 +91,13 @@ public class GUIController extends Application implements GUIControllerInterface
     } catch (IOException e) {
       e.printStackTrace();
     }
-    stage.setTitle("prova");
-    stage.setScene(new Scene(root));
+    background.setCenter(root);
     stage.show();
+
+  }
+
+  @Override
+  public void setController(GUIView controller) {
 
   }
 
